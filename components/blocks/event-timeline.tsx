7879059e52f0585844e4ac680bf8a019a26f1a12
Tom Benbow <tom@hellomoon.net>
2022-12-13 09:19:06 -0600

    Event timeline headline link

--- components/blocks/event-timeline.tsx
+++ components/blocks/event-timeline.tsx
@@ -1,4 +1,5 @@
-import React, {useState } from "react";
+import React from "react";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { Section } from "../section";
 import { Content } from "../content"
 import { minHeightOptions } from "../../schema/options"
@@ -6,6 +7,7 @@ import { backgroundSchema } from "../../schema/background"
 import { navigationLabelSchema } from "../../schema/navigation-label";
 
 export const EventTimeline = ({ data, events, parentField = "" }) => {
+  const styles = data.style
   const padding = data.style?.padding
   const width = data.style?.fullWidth ? "" : "max-w-desktop-full mx-auto"
   const sortedEvents = events.sort((a, b) => new Date(a.startDate).getTime() - new Date(b.startDate).getTime());
@@ -20,38 +22,36 @@ export const EventTimeline = ({ data, events, parentField = "" }) => {
       <div className={`${width} ${padding} ${data.style?.minHeight}`}>
         <div className="relative max-w-desktop-full mx-auto border-l border-primary mb-10 ml-60">
           {sortedEvents && sortedEvents.map((event, index) => {
+            const headline = event.website ? <a href={event.website} target="_blank">{event.eventName}</a> : event.eventName
             const startDate = new Date(event.startDate)
             const endDate = new Date(event.endDate)
             const startMonth = months[startDate?.getMonth()]
+            const date = `${startDate.toLocaleDateString()} - ${endDate.toLocaleDateString()}`
             const hideMonthLabel = labels.includes(startMonth)
             if (!hideMonthLabel) {
               labels = [...labels, startMonth]
             }
-            const contentData = {
-              label: `${startDate.toLocaleDateString()} - ${endDate.toLocaleDateString()}`,
-              headline: event.eventName,
-              subhead: event.location,
-              body: event.description,
-              buttons: event.website && [{
-                label: 'Details',
-                link: event.website,
-                buttonStyle: 'small',
-              }]
-            }
             return (
               <div className="mb-5 ml-4" key={index}>
-                {(!hideMonthLabel && 
+                {(!hideMonthLabel &&
                   <div className="absolute -left-36 w-32 text-right">{startMonth}</div>
                 )}
                 <div className="absolute w-3 h-3 bg-primary rounded-full mt-1.5 -left-1.5"></div>
-                <Content
-                  data = {contentData}
-                  styles = {data.style}
-                  alignment = "left"
-                  width = "w-full"
-                  parentField = {parentField}
-                  className = ""
-                />
+                {date &&
+                  <span className={`${styles.labelStyles}`} data-tinafield={`${parentField}.label`}>
+                    {date}
+                  </span>
+                }
+                {headline &&
+                  <h2 className={`${styles.headlineStyles}`} data-tinafield={`${parentField}.headline`}>
+                    {headline}
+                  </h2>
+                }
+                {event.location &&
+                  <h4 className={`${styles.subheadStyles}`} data-tinafield={`${parentField}.subhead`}>
+                    {event.location}
+                  </h4>
+                }
               </div>
             )
           })}
