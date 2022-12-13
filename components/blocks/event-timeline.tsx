d2816465f7135dcfc006b75f697e18c1c070c4e4
Tom Benbow <tom@hellomoon.net>
2022-12-13 10:14:25 -0600

    Add link icon

--- components/blocks/event-timeline.tsx
+++ components/blocks/event-timeline.tsx
@@ -6,6 +6,14 @@ import { minHeightOptions } from "../../schema/options"
 import { backgroundSchema } from "../../schema/background"
 import { navigationLabelSchema } from "../../schema/navigation-label";
 
+const IconLink = ({width="12"}) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width={width} className="relative -top-1 inline-block ml-0.5 fill-current">
+      <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/>
+    </svg>
+  )
+}
+
 export const EventTimeline = ({ data, events, parentField = "" }) => {
   const styles = data.style
   const padding = data.style?.padding
@@ -22,7 +30,7 @@ export const EventTimeline = ({ data, events, parentField = "" }) => {
       <div className={`${width} ${padding} ${data.style?.minHeight}`}>
         <div className="relative max-w-desktop-full mx-auto border-l border-primary mb-10 ml-60">
           {sortedEvents && sortedEvents.map((event, index) => {
-            const headline = event.website ? <a href={event.website} target="_blank">{event.eventName}</a> : event.eventName
+            const headline = event.website ? <a href={event.website} target="_blank">{event.eventName} <IconLink width="12" /></a> : event.eventName
             const startDate = new Date(event.startDate)
             const endDate = new Date(event.endDate)
             const startMonth = months[startDate?.getMonth()]
