b4f4a55b5e52f08b9268ce7756fd8edc9d7855d8
Tom Benbow <tom@hellomoon.net>
2023-01-11 22:13:57 -0600

    Update page tracking code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,18 @@ import React, { useEffect } from 'react';
 import Router from 'next/router';
 import * as Fathom from 'fathom-client';
 
-Router.events.on('routeChangeComplete', (as, routeProps) => {
-  if (!routeProps.shallow) {
-    Fathom.trackPageview()
-  }
-});
+const isBrowser = typeof window !== 'undefined';
+
+if(isBrowser) {
+  // For the first page load
+  Fathom.trackPageview()
+  // Subsequent route changes
+  Router.events.on('routeChangeComplete', (as, routeProps) => {
+    if (!routeProps.shallow) {
+      Fathom.trackPageview()
+    }
+  });
+}
 
 const App = ({ Component, pageProps }) => {
   // Initialize Fathom when the app loads
