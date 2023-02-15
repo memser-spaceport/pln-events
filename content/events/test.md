d1ddb3a41c0457d4b9fccf02670d326e25d8bcae
Aswin Raj <aswin@ideas2it.com>
2023-02-15 22:05:22 +0530

    Additional details for events

---
+++ content/events/test.md
@@ -0,0 +1,21 @@
+---
+eventName: Test FIL Web3 Event
+eventDescription: This is an FIL bangalore event
+website: 'https://google.com'
+eventOrg: Protocol Labs
+eventLogo: /uploads/protocol_labs.jpeg
+location: India
+venueName: Test
+venueMapsLink: 'https://goo.gl/maps/LvEbp436mAdmHGi89'
+venueAddress: Royapettah
+startDate: '2023-01-01T03:30:00.000Z'
+endDate: '2023-01-05T03:30:00.000Z'
+dri: Aswin Raj
+eventTopic:
+  - Web3
+  - Blockchain
+eventType: Conference
+tag: PLN Event
+juanSpeaking: 'no'
+---
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,27 +11,33 @@ export default function IndexPage(
     variables: props.variables,
     data: props.data,
   });
-  const eventsData = props.events.eventConnection.edges
-  const eventList = eventsData.map(event => {
-    return (
-      {
-        eventName: event.node?.eventName,
-        website: event.node?.website,
-        location: event.node?.location,
-        startDate: event.node?.startDate,
-        endDate: event.node?.endDate,
-        dateTBD: event.node?.dateTBD,
-        dri: event.node?.dri,
-        tag: event.node?.tag,
-        juanSpeaking: event.node?.juanSpeaking,
-      }
-    )
-  })
+  const eventsData = props.events.eventConnection.edges;
+  const eventList = eventsData.map((event) => {
+    return {
+      eventName: event.node?.eventName,
+      eventDescription: event.node?.eventDescription,
+      eventOrg: event.node?.eventOrg,
+      eventLogo: event.node?.eventLogo,
+      eventTopic: event.node?.eventTopic,
+      eventType: event.node?.eventType,
+      venueName: event.node?.venueName,
+      venueMapsLink: event.node?.venueMapsLink,
+      venueAddress: event.node?.venueAddress,
+      website: event.node?.website,
+      location: event.node?.location,
+      startDate: event.node?.startDate,
+      endDate: event.node?.endDate,
+      dateTBD: event.node?.dateTBD,
+      dri: event.node?.dri,
+      tag: event.node?.tag,
+      juanSpeaking: event.node?.juanSpeaking,
+    };
+  });
 
   return (
     /* TODO: needs ts type */
     <Layout rawData={data} data={data.global as any}>
-      <Blocks {...data.page } events={eventList} />
+      <Blocks {...data.page} events={eventList} />
     </Layout>
   );
 }
@@ -46,7 +52,7 @@ export const getStaticProps = async ({ params }) => {
       data: tinaProps.data,
       query: tinaProps.query,
       variables: tinaProps.variables,
-      events: eventsListData.data
+      events: eventsListData.data,
     },
   };
 };
