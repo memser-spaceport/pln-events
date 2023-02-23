c027752c8deb2d9c7bcbdbea578ab32fc72fea75
Thangaraj-Ideas2it <thangaraj.esakky@ideas2it.com>
2023-02-23 17:08:33 +0530

    Feat(Github Issue 17): Bug fixes

--- components/page/home/hp-sidebar.tsx
+++ components/page/home/hp-sidebar.tsx
@@ -7,13 +7,19 @@ import { getUniqueValuesFromEvents } from "./hp-helper";
 
 function HpSideBar(props) {
     const events = props.events ?? [];
+    const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+    const [selectedLocation, setSelectedLocation] = useState('All');
+    const [selectedTopic, setSelectedTopic] = useState('All')
+
     const filters = [
-        {name: "Year", type: 'select', items: getUniqueValuesFromEvents('startYear', [...events]), placeholder: 'Filter by year', defaultValue: new Date().getFullYear().toString(), dropdownImgUrl: '/icons/arrow-down-filled.svg', identifierId: 'year', iconUrl: '/icons/pl-calender-icon.svg'},
-        {name: "Location", type: 'select', items: getUniqueValuesFromEvents('location', [...events]), placeholder: 'Filter by location', defaultValue: 'All', dropdownImgUrl: '/icons/arrow-down-filled.svg', identifierId: 'location', iconUrl: '/icons/pl-location-icon.svg'},
+        {name: "Year", type: 'select', items: getUniqueValuesFromEvents('startYear', [...events]), selectedItem: selectedYear, onItemChange: setSelectedYear, placeholder: 'Filter by year', defaultValue: new Date().getFullYear().toString(), dropdownImgUrl: '/icons/arrow-down-filled.svg', identifierId: 'year', iconUrl: '/icons/pl-calender-icon.svg'},
+        {name: "Location", type: 'select', items: getUniqueValuesFromEvents('location', [...events]), selectedItem: selectedLocation, onItemChange: setSelectedLocation, placeholder: 'Filter by location', defaultValue: 'All', dropdownImgUrl: '/icons/arrow-down-filled.svg', identifierId: 'location', iconUrl: '/icons/pl-location-icon.svg'},
         {name: "Event Type", type: 'tags', items: ['Virtual', 'Conference', 'Social'], identifierId: 'eventType'},
-        {name: "Topics", type: 'select', items: getUniqueValuesFromEvents('topics', [...events]), placeholder:'Filter by topics', defaultValue: 'All', dropdownImgUrl: '/icons/arrow-down-filled.svg', identifierId: 'topic', iconUrl: '/icons/pl-topics-icon.svg'},
+        {name: "Topics", type: 'select', items: getUniqueValuesFromEvents('topics', [...events]), selectedItem: selectedTopic, onItemChange: setSelectedTopic, placeholder:'Filter by topics', defaultValue: 'All', dropdownImgUrl: '/icons/arrow-down-filled.svg', identifierId: 'topic', iconUrl: '/icons/pl-topics-icon.svg'},
        
     ]
+
+
 
     const onFilterChange = props.onFilterChange;
     const onClearFilters = () => {
@@ -22,9 +28,17 @@ function HpSideBar(props) {
         const topic = document.getElementById("topic-ps-input");
         const eventType = document.getElementById("eventType-pl-tag");
         const toggle = document.getElementById("isPlnEventOnly-pl-toggle")
-        yearElement.value = new Date().getFullYear().toString()
+
+        yearElement.value = new Date().getFullYear().toString();
+        setSelectedYear('')
+        setSelectedYear(new Date().getFullYear().toString())
+        setSelectedLocation('')
+        setSelectedLocation('All');
         locationElement.value = 'All';
+        setSelectedTopic('')
+        setSelectedTopic('All')
         topic.value = 'All'
+       
         eventType.value = ''
         toggle.checked = false;
         props.onClearFilters();
