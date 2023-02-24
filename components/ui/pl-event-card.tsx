3c4f88e2f701c8312c36cc85f68a281910ae002d
Thangaraj-Ideas2it <thangaraj.esakky@ideas2it.com>
2023-02-24 17:16:45 +0530

    Feat(Github Issue 17): Fix for topics UI

--- components/ui/pl-event-card.tsx
+++ components/ui/pl-event-card.tsx
@@ -36,8 +36,8 @@ function PlEventCard(props) {
             </div>
             {!website && <p className="pec__eventname">{eventName}</p>}
             {website && <p className="pec__eventname"><a className="blue" href={website} target="_blank"><span className="title">{eventName}</span></a></p>}
-            {topics.length > 0 && <div className="pec__topics">
-                {topics.map((v, vindex) => vindex <=3 ?<p className="pec__topics__item">{v}</p> : null )}
+            {(topics.length > 0) && <div className="pec__topics">
+                {topics.map(v => <p className="pec__topics__item">{v}</p> )}
             </div>}
 
             {description && <p className="pec__desc">{description}</p>}
