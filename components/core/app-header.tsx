ef2450c5d5f93e05a01e6daf416c903337a4c673
Thangaraj-Ideas2it <thangaraj.esakky@ideas2it.com>
2023-02-24 16:48:42 +0530

    Feat(Github Issue 17): Fix for review comments

--- components/core/app-header.tsx
+++ components/core/app-header.tsx
@@ -1,5 +1,9 @@
-function AppHeader() {
+import { trackGoal } from "fathom-client";
 
+function AppHeader() {
+    const onHostEventClicked = () => {
+        trackGoal('CXB9QJA8', 0)
+    }
     return <>
         <header className="ah">
             <div className="ah__logo">
@@ -8,7 +12,7 @@ function AppHeader() {
                     <h1 className="ah__logo__heading__text">Protocol Labs</h1>
                     <h1 className="ah__logo__heading__text">Network Events</h1>
                 </div></div>
-            <a className="ah__btn" target="_blank" href="https://github.com/memser-spaceport/pln-events#submitting-events">Host an event</a>
+            <a className="ah__btn" onClick={onHostEventClicked} target="_blank" href="https://github.com/memser-spaceport/pln-events#submitting-events">Host an event</a>
         </header>
         <style jsx>
             {
@@ -21,7 +25,7 @@ function AppHeader() {
             .ah__logo__heading { margin:0; padding:0; line-height: 18px; font-size: 18px; font-weight: 700; margin-left: 8px;}
             .ah__logo__heading__text {margin:0; padding:0; font-size: 16px;}
             
-            .ah__btn {cursor: pointer; text-decoration: none;  box-shadow: 0px 1px 1px rgba(7, 8, 8, 0.16), inset 0px 1px 0px rgba(255, 255, 255, 0.16); padding: 8px 16px;background:linear-gradient(90deg, #427DFF 0%, #44D5BB 100%); border-radius: 20px; color: white; outline: none; border: none;}
+            .ah__btn {cursor: pointer; text-decoration: none;  box-shadow: 0px 1px 1px rgba(7, 8, 8, 0.16), inset 0px 1px 0px rgba(255, 255, 255, 0.16); padding: 8px 24px;background:linear-gradient(90deg, #427DFF 0%, #44D5BB 100%); border-radius: 20px; color: white; outline: none; border: none; font-size: 15px; font-weight: 600;}
             
             `
             }
