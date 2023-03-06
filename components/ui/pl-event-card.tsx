890ab9ffdce846213283002f5735b34435435410
Thangaraj-Ideas2it <thangaraj.esakky@ideas2it.com>
2023-03-06 03:15:39 -0800

    Feat(Github-43): Removed test file

--- components/ui/pl-event-card.tsx
+++ components/ui/pl-event-card.tsx
@@ -52,9 +52,9 @@ function PlEventCard(props) {
             <div className={`pec__topics`}>
                 {trimmedTopics.map(v => <p className="pec__topics__item">{v}</p>)}
             </div>
-            {(preferredContacts.length > 0) && <div className="pec__contacts">
+            <div className="pec__contacts">
                     {preferredContacts.map(c => <a className="pec__contacts__link" href={c.link} target="_blank"><img title={c.name} className="pec__contacts__link__img" src={c.logo}/></a>)}
-                </div>}
+                </div>
 
             {description && <p className="pec__desc">{description}</p>}
             <div className="pec__calender">
