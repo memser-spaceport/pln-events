acec322f0e03365bb791acd41de77020110f03ae
NivedhaSV <nivedha.selvaraja@ideas2it.com>
2024-09-25 15:22:56 +0530

    build fix

--- app/api/eventjson/route.ts
+++ app/api/eventjson/route.ts
@@ -2,20 +2,7 @@ import { NextRequest } from "next/server";
 import fs from "fs";
 import path from "path";
 
-export async function GET(request: NextRequest) {
-  try {
-    // const eventsListData = await client.queries.eventConnection({ last: -1 });
-    // const events = eventsListData?.data?.eventConnection?.edges ?? [];
-    // return Response.json({ data: events }, { status: 200 });
-    const jsonData = readJsonFiles(); // Call the utility function with your folder path
-    return Response.json({ data: jsonData }, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return Response.json({ message: "Internal Server Error" }, { status: 500 });
-  }
-}
-
-export function readJsonFiles() {
+function readJsonFiles() {
   const folderPath = path.join(process.cwd(), "events"); // Get the absolute path to the folder
   const fileNames = fs.readdirSync(folderPath); // Read all file names in the folder
 
@@ -30,3 +17,16 @@ export function readJsonFiles() {
 
   return jsonData;
 }
+export async function GET(request: NextRequest) {
+  try {
+    // const eventsListData = await client.queries.eventConnection({ last: -1 });
+    // const events = eventsListData?.data?.eventConnection?.edges ?? [];
+    // return Response.json({ data: events }, { status: 200 });
+    const jsonData = readJsonFiles(); // Call the utility function with your folder path
+    return Response.json({ data: jsonData }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return Response.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+}
+
